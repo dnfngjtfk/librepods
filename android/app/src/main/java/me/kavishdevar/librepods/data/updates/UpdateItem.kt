12abfb9fff32439e3fee774b0a15ee5a166922ff
dnfngjtfk <sanamolavko@gmail.com>
2026-08-16 11:26:11 +0300

    почистил хуйню

--- android/app/src/main/java/me/kavishdevar/librepods/data/updates/UpdateItem.kt
+++
@@ -1,9 +0,0 @@
-package me.kavishdevar.librepods.data.updates
-
-import androidx.compose.runtime.Composable
-
-data class UpdateItem(
-    val titleRes: Int,
-    val descriptionRes: Int,
-    val demoComposeable: @Composable () -> Unit
-)
